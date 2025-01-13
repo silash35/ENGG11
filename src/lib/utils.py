d8532a5b5a47938e5dfd52b3f5c54655ea38c51a
Silas Henrique <silash35@gmail.com>
2025-01-12 23:41:18 -0300

    Move more code to other files

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,5 +1,3 @@
-import time
-
 import torch
 
 
@@ -13,8 +11,24 @@ def mean_square(x):
     return torch.mean(x**2)
 
 
-def timer(func, *args):
-    start_time = time.monotonic()
-    result = func(*args)
-    elapsed_time = time.monotonic() - start_time
-    return result, elapsed_time
+def train(
+    model, loss_fn, optimizer, epochs, t: torch.Tensor, target_loss: None | float = None
+):
+    loss_value = 0
+    for _ in range(epochs):
+        # Coloca o modelo no modo de treinamento
+        model.train()
+
+        # Calcula o loss usando a nossa função loss.
+        loss = loss_fn(model, t)
+
+        # Ajusta os valores do modelo
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        loss_value = float(loss.detach().numpy())
+        if target_loss is not None and loss_value < target_loss:
+            break
+
+    return loss_value
